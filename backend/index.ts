42161708aea77f5791ce0ef716a12b13e990443d
Young Seo Kim <youngseokim7@gmail.com>
2025-06-25 22:07:51 +0900

    initial setup

--- backend/index.ts
+++ backend/index.ts
@@ -1,1 +1,27 @@
-console.log("Hello via Bun!");
+import express from "express";
+import cors from "cors";
+import { createServer } from "node:http";
+import { Server, Socket } from "socket.io";
+
+const app = express();
+const server = createServer(app);
+const port = process.env.PORT;
+if (!port) {
+  throw new Error("port not defined in secrets");
+}
+
+app.use(cors());
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.send("<h1>Hello world</h1>");
+});
+
+server.listen(port, () => {
+  console.log(`lisetning on port: ${port}`);
+});
+
