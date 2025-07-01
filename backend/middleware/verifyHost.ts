4ba8d0ed02d80be972c19898ecfc2e42f507cdf6
Young Seo Kim <youngseokim7@gmail.com>
2025-07-01 17:19:06 +0900

    move middleware to seperate file

---
+++ backend/middleware/verifyHost.ts
@@ -0,0 +1,29 @@
+import express from "express";
+import { redisClient } from "../index.js";
+
+export const verifyHost = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction,
+) => {
+  try {
+    const { roomId } = req.params;
+    const { hostKey } = req.body;
+
+    if (!hostKey) {
+      res.status(401).json({ error: "no host key provided" });
+      return;
+    }
+
+    const roomHostKey = await redisClient.hGet(`room:${roomId}`, "hostKey");
+    if (hostKey !== roomHostKey) {
+      res.status(403).json({ error: "invalid host key" });
+      return;
+    }
+
+    next();
+  } catch (error) {
+    res.status(500).json({ error: "host key verification failed" });
+    return;
+  }
+};
--- backend/routes/hostRoutes.ts
+++ backend/routes/hostRoutes.ts
@@ -6,32 +6,7 @@ type RoomState = "nominating" | "voting" | "done";
 
 const router = express.Router();
 
-const verifyHost = async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction,
-) => {
-  try {
-    const { roomId } = req.params;
-    const { hostKey } = req.body;
-
-    if (!hostKey) {
-      res.status(401).json({ error: "no host key provided" });
-      return;
-    }
-
-    const roomHostKey = await redisClient.hGet(`room:${roomId}`, "hostKey");
-    if (hostKey !== roomHostKey) {
-      res.status(403).json({ error: "invalid host key" });
-      return;
-    }
-
-    next();
-  } catch (error) {
-    res.status(500).json({ error: "host key verification failed" });
-    return;
-  }
-};
+import { verifyHost } from "../middleware/verifyHost.js";
 
 interface createBody {
   roomName: string;
@@ -156,4 +131,3 @@ router.post(
 );
 
 export { router };
-
