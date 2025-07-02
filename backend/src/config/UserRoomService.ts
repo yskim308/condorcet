4277f7e9d6a40bf362fc863f77f3669720b1f5db
Young Seo Kim <youngseokim7@gmail.com>
2025-07-02 19:07:16 +0900

    initial implementation of UserRoomService

---
+++ backend/src/config/UserRoomService.ts
@@ -0,0 +1,19 @@
+import { redisClient } from "./redisClient";
+import { getErrorMessage, getRedisError } from "../util/getErrorMessage";
+
+export class UserRoomService {
+  async enrollUser(
+    roomId: string,
+    userName: string,
+  ): Promise<[Error | null, number]> {
+    try {
+      await redisClient.sAdd(`room:${roomId}:users`, userName);
+      return [null, 200];
+    } catch (error: unknown) {
+      console.error("error adding user to room: " + getErrorMessage(error));
+      return getRedisError(error);
+    }
+  }
+
+  // @toodo: userExists
+}
