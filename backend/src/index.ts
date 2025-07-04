d25f41b2ae5939c66b565398e61eb46d51119282
Young Seo Kim <youngseokim7@gmail.com>
2025-07-04 16:19:05 +0900

    instantiate socket service in main

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
 import { createServer } from "node:http";
-import { Server, Socket } from "socket.io";
+import { Server } from "socket.io";
+import SocketService from "./config/SocketService";
 import { router as hostRoutes } from "./routes/hostRoutes";
 import { router as participantRoutes } from "./routes/participantRoutes";
 
@@ -16,27 +17,16 @@ app.use(cors());
 app.use(express.json());
 app.use("/host", hostRoutes);
 app.use("/participant", participantRoutes);
-export const io = new Server(server, {
+const io = new Server(server, {
   cors: {
     origin: "*",
   },
 });
 
+export const socketService = new SocketService(io);
+
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("<h1>Hello world</h1>");
-});
-
-io.on("connection", (socket: Socket) => {
-  console.log("User connected:", socket.id);
-
-  socket.on("join-room", async (roomId: string) => {
-    await socket.join(roomId);
-    console.log(`Socket ${socket.id} joined room ${roomId}`);
-  });
-
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
 });
 
 server.listen(port, () => {
