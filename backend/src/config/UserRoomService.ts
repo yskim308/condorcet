2ff7a5faafe6ba6a97ab4eda3c06058da3621cc2
Young Seo Kim <youngseokim7@gmail.com>
2025-07-15 15:45:25 +0900

    inject Redis Client as a dependency

--- backend/src/config/UserRoomService.ts
+++ backend/src/config/UserRoomService.ts
@@ -1,13 +1,20 @@
 import { redisClient } from "./redisClient";
+import type { RedisClientType } from "redis";
 import { getErrorMessage, getRedisError } from "../util/getErrorMessage";
 
 class UserRoomService {
+  private redisClient: RedisClientType;
+
+  constructor(redisClient: RedisClientType) {
+    this.redisClient = redisClient;
+  }
+
   async enrollUser(
     roomId: string,
     userName: string,
   ): Promise<[Error | null, number]> {
     try {
-      await redisClient.sAdd(`room:${roomId}:users`, userName);
+      await this.redisClient.sAdd(`room:${roomId}:users`, userName);
       return [null, 200];
     } catch (error: unknown) {
       console.error("error adding user to room: " + getErrorMessage(error));
@@ -20,7 +27,7 @@ class UserRoomService {
     userName: string,
   ): Promise<[Error | null, boolean, number]> {
     try {
-      const exists = await redisClient.sIsMember(
+      const exists = await this.redisClient.sIsMember(
         `room:${roomId}:users`,
         userName,
       );
@@ -40,7 +47,7 @@ class UserRoomService {
 
   async getUsers(roomId: string): Promise<[Error | null, string[], number]> {
     try {
-      const members: string[] = await redisClient.sMembers(
+      const members: string[] = await this.redisClient.sMembers(
         `room:${roomId}:users`,
       );
       return [null, members, 200];
@@ -65,7 +72,7 @@ class UserRoomService {
           404,
         ];
       }
-      await redisClient.sAdd(`room:${roomId}:voted`, userName);
+      await this.redisClient.sAdd(`room:${roomId}:voted`, userName);
       return [null, 200];
     } catch (error: unknown) {
       return getRedisError(error);
@@ -93,5 +100,4 @@ class UserRoomService {
   }
 }
 
-const userRoomService = new UserRoomService();
-export default userRoomService;
+export default UserRoomService;
