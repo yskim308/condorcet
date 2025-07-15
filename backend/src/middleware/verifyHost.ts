05ee17578cfe6211eca1538ba779546306bdc892
Young Seo Kim <youngseokim7@gmail.com>
2025-07-15 15:48:13 +0900

    return middleware factory with injected dependency

--- backend/src/middleware/verifyHost.ts
+++ backend/src/middleware/verifyHost.ts
@@ -1,38 +1,41 @@
-import express from "express";
-import roomService from "../config/RoomService";
+import type express from "express";
+import type RoomService from "../config/RoomService";
 
-export const verifyHost = async (
-  req: express.Request,
-  res: express.Response,
-  next: express.NextFunction,
-) => {
-  try {
-    const { roomId } = req.params;
-    const { hostKey } = req.body;
+export const createVerifyHostMiddleware = (roomService: RoomService) => {
+  return async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ) => {
+    try {
+      const { roomId } = req.params;
+      const { hostKey } = req.body;
 
-    if (!hostKey) {
-      res.status(401).json({ error: "no host key provided" });
+      if (!hostKey) {
+        res.status(401).json({ error: "no host key provided" });
+        return;
+      }
+
+      const [err, roomHostKey, code] = await roomService.getHostKey(roomId);
+      if (err) {
+        console.error(
+          `Error getting hostkey for room: ${roomId}: ${err.message}`,
+        );
+        res
+          .status(code)
+          .json({ error: `redis failde to get hostkey: ${err.message}` });
+        return;
+      }
+
+      if (hostKey !== roomHostKey) {
+        res.status(403).json({ error: "invalid host key" });
+        return;
+      }
+
+      next();
+    } catch (error) {
+      res.status(500).json({ error: "host key verification failed" });
       return;
     }
-
-    const [err, roomHostKey, code] = await roomService.getHostKey(roomId);
-    if (err) {
-      console.error(
-        `Error getting hostkey for room: ${roomId}: ${err.message}`,
-      );
-      res
-        .status(code)
-        .json({ error: `redis failde to get hostkey: ${err.message}` });
-    }
-
-    if (hostKey !== roomHostKey) {
-      res.status(403).json({ error: "invalid host key" });
-      return;
-    }
-
-    next();
-  } catch (error) {
-    res.status(500).json({ error: "host key verification failed" });
-    return;
-  }
+  };
 };
