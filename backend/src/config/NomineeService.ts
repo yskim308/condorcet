e5a4c42639488b44887c91a11123d86881836f20
Young Seo Kim <youngseokim7@gmail.com>
2025-07-28 21:23:17 +0900

    refactor to use localized error handling"

--- backend/src/config/NomineeService.ts
+++ backend/src/config/NomineeService.ts
@@ -10,179 +10,102 @@ export default class NomineeService {
   constructor(redisClient: RedisClientType) {
     this.redisClient = redisClient;
   }
-  async setNomineeCount(roomId: string): Promise<[Error | null, number]> {
+  async setNomineeCount(roomId: string) {
     const key = `room:${roomId}:nominee_count`;
-    try {
-      await this.redisClient.set(key, -1);
-      await this.redisClient.expire(key, TTL);
-      return [null, 200];
-    } catch (error: unknown) {
-      console.error("error setting nominee count: " + getErrorMessage(error));
-      return getRedisError(error);
-    }
+    await this.redisClient.set(key, -1);
+    await this.redisClient.expire(key, TTL);
   }
 
-  async getNomineeCount(
-    roomId: string,
-  ): Promise<[Error | null, number | null, number]> {
-    try {
-      const count = await this.redisClient.get(`room:${roomId}:nominee_count`);
-      if (!count) {
-        return [null, null, 200];
-      }
-      return [null, parseInt(count), 200];
-    } catch (error: unknown) {
-      console.error("error getting nominee count: " + getErrorMessage(error));
-      const [err, code] = getRedisError(error);
-      return [err, null, code];
+  async getNomineeCount(roomId: string) {
+    const count = await this.redisClient.get(`room:${roomId}:nominee_count`);
+    if (!count) {
+      return 0;
     }
+    return parseInt(count);
   }
 
-  async addNominee(
-    roomId: string,
-    nominee: string,
-  ): Promise<[Error | null, number]> {
+  async addNominee(roomId: string, nominee: string) {
     const key = `room:${roomId}:nominees`;
-    try {
-      const nominee_id = await this.redisClient.incr(
-        `room:${roomId}:nominee_count`,
-      );
-      await this.redisClient.hSet(
-        key,
-        nominee_id,
-        nominee,
-      );
-      await this.redisClient.expire(key, TTL);
-      return [null, 200];
-    } catch (error: unknown) {
-      console.error("error adding nominee: " + getErrorMessage(error));
-      return getRedisError(error);
-    }
+    const nominee_id = await this.redisClient.incr(
+      `room:${roomId}:nominee_count`,
+    );
+    await this.redisClient.hSet(key, nominee_id, nominee);
+    await this.redisClient.expire(key, TTL);
   }
 
-  async getAllNominees(
-    roomId: string,
-  ): Promise<[Error | null, NomineeMap | null, number]> {
-    try {
-      // HGETALL returns an object where keys are fields and values are their corresponding values.
-      const nominees: NomineeMap = await this.redisClient.hGetAll(
-        `room:${roomId}:nominees`,
-      );
-
-      return [null, nominees, 200];
-    } catch (error: unknown) {
-      console.error("error getting all nominees: " + getErrorMessage(error));
-      // Depending on your error handling, you might return a specific error type;
-      const [err, code] = getRedisError(error);
-      return [err, null, code];
-    }
+  async getAllNominees(roomId: string): Promise<NomineeMap> {
+    // HGETALL returns an object where keys are fields and values are their corresponding values.
+    const nominees: NomineeMap = await this.redisClient.hGetAll(
+      `room:${roomId}:nominees`,
+    );
+    if (!nominees) return {};
+    return nominees;
   }
 
-  async saveVote(
-    roomId: string,
-    preferences: string[],
-  ): Promise<[Error | null, number]> {
+  async saveVote(roomId: string, preferences: string[]) {
     const key = `room:${roomId}:votes`;
-    try {
-      // Serialize the array to a JSON string to preserve its structure
-      const serializedPreferences = JSON.stringify(preferences);
-      await this.redisClient.lPush(
-        key,
-        serializedPreferences,
-      );
-      await this.redisClient.expire(key, TTL);
-      return [null, 200];
-    } catch (error: unknown) {
-      console.error(
-        "Error saving voting preferences: " + getErrorMessage(error),
-      );
-      return getRedisError(error);
-    }
+    // Serialize the array to a JSON string to preserve its structure
+    const serializedPreferences = JSON.stringify(preferences);
+    await this.redisClient.lPush(key, serializedPreferences);
+    await this.redisClient.expire(key, TTL);
   }
 
-  async getAllVotes(
-    roomId: string,
-  ): Promise<[Error | null, string[][] | null, number]> {
-    try {
-      const votesAsJson = await this.redisClient.lRange(
-        `room:${roomId}:votes`,
-        0,
-        -1,
-      );
-      if (!votesAsJson) {
-        return [null, [], 200]; // Return empty array if no votes
-      }
-      // Parse each JSON string back into an array of strings
-      const votes = votesAsJson.map((vote) => JSON.parse(vote));
-      return [null, votes, 200];
-    } catch (error: unknown) {
-      console.error("error getting all votes: " + getErrorMessage(error));
-      const [err, code] = getRedisError(error);
-      return [err, null, code];
+  async getAllVotes(roomId: string): Promise<string[][]> {
+    const votesAsJson = await this.redisClient.lRange(
+      `room:${roomId}:votes`,
+      0,
+      -1,
+    );
+    if (!votesAsJson) {
+      return [[]];
     }
+    // Parse each JSON string back into an array of strings
+    const votes = votesAsJson.map((vote) => JSON.parse(vote));
+    return votes;
   }
 
-  async tallyVotes(roomId: string): Promise<[Error | null, string, number]> {
-    try {
-      const votesAsJson = await this.redisClient.lRange(
-        `room:${roomId}:votes`,
-        0,
-        -1,
-      );
-      // Parse each JSON string back into an array of strings
-      const votes = votesAsJson.map((vote) => JSON.parse(vote));
-      if (!votes || votes.length === 0) {
-        return [new Error("no votes recorded"), "", 200];
-      }
-
-      // get nominee count
-      const nomineeCount = await this.redisClient.get(
-        `room:${roomId}:nominee_count`,
-      );
-      if (!nomineeCount) {
-        return [new Error("nominee count not initialized"), "", 500];
-      }
-
-      // find winner with util, then set in redis
-      const winnerId = findWinner(votes, parseInt(nomineeCount));
-      if (winnerId < 0) {
-        return [new Error("winnerID not valid"), "", 500];
-      }
-
-      // get the nominee map to find winner string
-      const nomineeMap = await this.redisClient.hGetAll(
-        `room:${roomId}:nominees`,
-      );
-      const winner: string = nomineeMap[String(winnerId)];
-      if (!winner) {
-        return [new Error("winner Id not defined in map"), "", 500];
-      }
-
-      await this.redisClient.hSet(`room:${roomId}`, "winnerId", winnerId);
-      await this.redisClient.hSet(`room:${roomId}`, "winnerName", winner);
-      return [null, winner, 200];
-    } catch (error: unknown) {
-      console.error("error when determinig winner: " + getErrorMessage(error));
-      const [err, code] = getRedisError(error);
-      return [err, "", code];
+  async tallyVotes(roomId: string): Promise<string> {
+    const votesAsJson = await this.redisClient.lRange(
+      `room:${roomId}:votes`,
+      0,
+      -1,
+    );
+    // Parse each JSON string back into an array of strings
+    const votes = votesAsJson.map((vote) => JSON.parse(vote));
+    if (!votes || votes.length === 0) {
+      throw new Error(`no recorded votes when tallying room ${roomId}`);
     }
+
+    // get nominee count
+    const nomineeCount = await this.redisClient.get(
+      `room:${roomId}:nominee_count`,
+    );
+    if (!nomineeCount) {
+      throw new Error(`nominee count not initialized in room ${roomId}`);
+    }
+
+    // find winner with util, then set in redis
+    const winnerId = findWinner(votes, parseInt(nomineeCount));
+    if (winnerId < 0) {
+      throw new Error(`winnerID ${winnerId} not valid in room ${roomId}`);
+    }
+
+    // get the nominee map to find winner string
+    const nomineeMap = await this.redisClient.hGetAll(
+      `room:${roomId}:nominees`,
+    );
+    const winner: string = nomineeMap[String(winnerId)];
+    if (!winner) {
+      throw new Error(`winner Id not defined in map for ${roomId}`);
+    }
+
+    await this.redisClient.hSet(`room:${roomId}`, "winnerId", winnerId);
+    await this.redisClient.hSet(`room:${roomId}`, "winnerName", winner);
+    return winner;
   }
 
-  async getWinner(roomId: string): Promise<[Error | null, string, number]> {
-    try {
-      const winner = await this.redisClient.hGet(
-        `room:${roomId}`,
-        "winnerName",
-      );
-      if (!winner) {
-        return [new Error("no winner was set"), "", 401];
-      } else {
-        return [null, winner, 200];
-      }
-    } catch (error: unknown) {
-      console.error("error when getting winner: " + getErrorMessage(error));
-      const [err, code] = getRedisError(error);
-      return [err, "", code];
-    }
+  async getWinner(roomId: string): Promise<string> {
+    const winner = await this.redisClient.hGet(`room:${roomId}`, "winnerName");
+    return winner ? winner : "";
   }
 }
