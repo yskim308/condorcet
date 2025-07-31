62e6149e1c8d0131446bf8ddd33e43c32440177c
Young Seo Kim <youngseokim7@gmail.com>
2025-07-31 16:42:44 +0900

    move to global erro handling on participant routes

--- backend/src/routes/participantRoutes.ts
+++ backend/src/routes/participantRoutes.ts
@@ -18,7 +18,11 @@ export const createParticipantRouter = (
   }
   router.post(
     "/room/join",
-    async (req: express.Request, res: express.Response) => {
+    async (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction,
+    ) => {
       try {
         const { roomId, userName }: joinBody = req.body;
 
@@ -28,31 +32,14 @@ export const createParticipantRouter = (
         }
 
         // check if room exists
-        const [existsErr, roomExists, codeExists] =
-          await roomService.exists(roomId);
-        if (existsErr) {
-          res.status(codeExists).json({
-            error: `Failed to check if room exists: ${existsErr.message}`,
-          });
-          return;
-        }
+        const roomExists = await roomService.exists(roomId);
         if (!roomExists) {
           res.status(404).json({ error: "Room not found" });
           return;
         }
 
         // 'join' the user to the room
-        const [enrollErr, codeEnroll] = await userRoomService.enrollUser(
-          roomId,
-          userName,
-        );
-        if (enrollErr) {
-          res
-            .status(codeEnroll)
-            .json({ error: `Failed to join room: ${enrollErr.message}` });
-          return;
-        }
-
+        await userRoomService.enrollUser(roomId, userName);
         socketService.emitNewUser(roomId, userName);
 
         res.status(200).json({
@@ -61,8 +48,7 @@ export const createParticipantRouter = (
           userName,
         });
       } catch (error) {
-        console.error("Error joining room:", error);
-        res.status(500).json({ error: "Failed to join room" });
+        next(error);
       }
     },
   );
@@ -72,18 +58,16 @@ export const createParticipantRouter = (
   }
   router.post(
     "/room/:roomId/getRoomData",
-    async (req: express.Request, res: express.Response) => {
+    async (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction,
+    ) => {
       try {
         const { roomId } = req.params;
         const { userName }: GetRoomDataBody = req.body;
         // check if the room exists
-        const [existErr, exists, existCode] = await roomService.exists(roomId);
-        if (existErr) {
-          res.status(existCode).json({
-            error: `error when checking if room exists: ${existErr.message}`,
-          });
-          return;
-        }
+        const exists = await roomService.exists(roomId);
         if (!exists) {
           res.status(401).json({
             error: "room does not exist",
@@ -92,35 +76,11 @@ export const createParticipantRouter = (
         }
 
         // get the role
-        const [roleErr, role, roleCode] = await userRoomService.getRole(
-          roomId,
-          userName,
-        );
-        if (roleErr) {
-          res.status(roleCode).json({
-            error: `Error while getting role of user ${userName}: ${roleErr.message}`,
-          });
-          return;
-        }
-
+        const role = await userRoomService.getRole(roomId, userName);
         // get users
-        const [userErr, users, userCode] =
-          await userRoomService.getUsers(roomId);
-        if (userErr) {
-          res.status(userCode).json({
-            error: `error whiel getting users in room: ${userErr.message}`,
-          });
-          return;
-        }
-
+        const users = await userRoomService.getUsers(roomId);
         // get the state
-        const [stateErr, state, stateCode] = await roomService.getState(roomId);
-        if (stateErr) {
-          res.status(stateCode).json({
-            error: `error while getting state of room ${roomId}: ${stateErr?.message}`,
-          });
-          return;
-        }
+        const state = await roomService.getState(roomId);
         if (!state) {
           res.status(500).json({
             error: "state is null",
@@ -129,14 +89,7 @@ export const createParticipantRouter = (
         }
 
         // get nominations
-        const [nominationErr, nominations, nominationCode] =
-          await nomineeService.getAllNominees(roomId);
-        if (nominationErr) {
-          res.status(nominationCode).json({
-            error: `error while getting nominations in room: ${nominationErr.message}`,
-          });
-          return;
-        }
+        const nominations = await nomineeService.getAllNominees(roomId);
         if (!nominations) {
           res.status(500).json({
             error: "nominations are null",
@@ -148,26 +101,11 @@ export const createParticipantRouter = (
         let winnerResponse: string | undefined;
 
         if (state === "voting") {
-          const [votedErr, votedUsers, votedCode] =
-            await userRoomService.getVotedUsers(roomId);
-          if (votedErr) {
-            res.status(votedCode).json({
-              error: `Error while getting voted users: ${votedErr.message}`,
-            });
-            return;
-          }
+          const votedUsers = await userRoomService.getVotedUsers(roomId);
           votedUsersResponse = votedUsers;
         }
-
         if (state === "done") {
-          const [winnerErr, winner, winnerCode] =
-            await nomineeService.getWinner(roomId);
-          if (winnerErr) {
-            res.status(winnerCode).json({
-              error: `Error while getting the winner: ${winnerErr.message}`,
-            });
-            return;
-          }
+          const winner = await nomineeService.getWinner(roomId);
           winnerResponse = winner;
         }
 
@@ -181,13 +119,14 @@ export const createParticipantRouter = (
         if (votedUsersResponse) {
           responseData.votedUsers = votedUsersResponse;
         }
-
         if (winnerResponse) {
           responseData.winner = winnerResponse;
         }
 
         res.status(200).json(responseData);
-      } catch (error: unknown) {}
+      } catch (error: unknown) {
+        next(error);
+      }
     },
   );
 
@@ -197,41 +136,31 @@ export const createParticipantRouter = (
   }
   router.post(
     "/room/:roomId/vote",
-    async (req: express.Request, res: express.Response) => {
+    async (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction,
+    ) => {
       try {
         const { vote, userName }: VoteBody = req.body;
         const { roomId } = req.params;
 
         // check if user already voted
-        const [checkErr, voted, checkCode] =
-          await userRoomService.checkUserVoted(roomId, userName);
-        if (checkErr) {
-          res.status(checkCode).json({
-            error: `failed to check if user voted: ${checkErr.message}`,
-          });
-          return;
-        }
+        const voted = await userRoomService.checkUserVoted(roomId, userName);
         if (voted) {
           res.status(404).json({ error: "user already voted" });
           return;
         }
 
         // set the vote in redis
-        const [voteErr, voteCode] = await nomineeService.saveVote(roomId, vote);
-        if (voteErr) {
-          res
-            .status(voteCode)
-            .json({ error: `failed to save vote: ${voteErr.message}` });
-        }
-
+        await nomineeService.saveVote(roomId, vote);
         // emit on socket
         socketService.emitNewVote(roomId, userName);
 
         res.status(200).json({ message: "vote saved" });
         return;
       } catch (error: unknown) {
-        console.error("error posting vote: ", error);
-        res.status(500).json({ error: "failed to register vote" });
+        next(error);
       }
     },
   );
