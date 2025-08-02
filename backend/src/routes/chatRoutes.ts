258bb28ad10e42f95693f0e4061e3058fcf00b4a
Young Seo Kim <youngseokim7@gmail.com>
2025-08-02 10:49:33 +0900

    implement chat routes

---
+++ backend/src/routes/chatRoutes.ts
@@ -0,0 +1,52 @@
+import express from "express";
+import ChatService from "../config/ChatService";
+import type SocketService from "../config/SocketService";
+import type { Message } from "../types/chat";
+
+export const createChatRouter = (
+  socketSerivce: SocketService,
+  chatService: ChatService,
+) => {
+  const router = express.Router();
+  router.get(
+    "/rooms/:roomId/message/getAll",
+    async (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction,
+    ) => {
+      try {
+        const { roomId } = req.params;
+        const messages: Message[] = await chatService.getAllMessages(roomId);
+        res.status(200).send(messages);
+      } catch (error) {
+        next(error);
+      }
+    },
+  );
+
+  interface SendMessageBody {
+    userName: string;
+    message: string;
+  }
+  router.post(
+    "/rooms/:roomId/message/sendMessage",
+    async (
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction,
+    ) => {
+      try {
+        const { roomId } = req.params;
+        const { userName, message }: SendMessageBody = req.body;
+        await chatService.sendMessage(roomId, userName, message);
+        res
+          .status(200)
+          .json({ message: `message sent succesfully to room:${roomId}` });
+      } catch (error) {
+        next(error);
+      }
+    },
+  );
+  return router;
+};
