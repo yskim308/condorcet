a25c66f4868e6825226f608e72e17c931086ec7d
Young Seo Kim <youngseokim7@gmail.com>
2025-08-03 17:47:51 +0900

    update index for imports and error hadnlign middleware usage

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,10 +8,13 @@ import SocketService from "./config/SocketService";
 import NomineeService from "./config/NomineeService";
 import UserRoomService from "./config/UserRoomService";
 import RoomService from "./config/RoomService";
+import ChatService from "./config/ChatService";
 import { createVerifyHostMiddleware } from "./middleware/verifyHost";
 import { redisClient } from "./config/redisClient";
 import { createHostRouter } from "./routes/hostRoutes";
 import { createParticipantRouter } from "./routes/participantRoutes";
+import { createChatRouter } from "./routes/chatRoutes";
+import globalErrorHandler from "./middleware/globalErrorHandler";
 
 const app = express();
 const server = createServer(app);
@@ -30,6 +33,7 @@ const socketService = new SocketService(io);
 const roomService = new RoomService(redisClient);
 const userRoomService = new UserRoomService(redisClient);
 const nomineeService = new NomineeService(redisClient);
+const chatService = new ChatService(redisClient);
 
 const hostRoutes = createHostRouter(
   socketService,
@@ -46,14 +50,14 @@ const participantRoutes = createParticipantRouter(
   nomineeService,
 );
 
+const chatRoutes = createChatRouter(socketService, chatService);
+
 app.use(cors());
 app.use(express.json());
-app.use("/host", hostRoutes);
-app.use("/participant", participantRoutes);
-
-app.get("/", (req: express.Request, res: express.Response) => {
-  res.send("<h1>Hello world</h1>");
-});
+app.use(hostRoutes);
+app.use(participantRoutes);
+app.use(chatRoutes);
+app.use(globalErrorHandler);
 
 server.listen(port, () => {
   console.log(`lisetning on port: ${port}`);
