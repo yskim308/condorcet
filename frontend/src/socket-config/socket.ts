5026a5f574229e700c9bd1889081a369438a296a
Young Seo Kim <youngseokim7@gmail.com>
2025-08-10 03:00:18 +0900

    explicitly type server and client event interactions

--- frontend/src/socket-config/socket.ts
+++ frontend/src/socket-config/socket.ts
@@ -1,9 +1,22 @@
-import { io } from "socket.io-client";
+import { io, Socket } from "socket.io-client";
 
 const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL;
 if (!backendURL) {
   throw new Error("error, backend url not set in .env");
 }
-const socket = io(backendURL);
+interface ServerToClientEvents {
+  "new-nomination": (payload: { nominee: string; roomId: string }) => void;
+  "state-change": (payload: { state: string; roomId: string }) => void;
+  "new-user": (payload: { state: string; roomId: string }) => void;
+  "user-voted": (payload: { userName: string }) => void;
+  "new-message": (payload: { userName: string; message: string }) => void;
+  winner: (winner: string) => void;
+}
+
+interface ClientToServerEvents {
+  "join-room": (roomId: string) => void;
+}
+const socket: Socket<ServerToClientEvents, ClientToServerEvents> =
+  io(backendURL);
 
 export default socket;
