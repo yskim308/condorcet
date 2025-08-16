3094e3b77da7513457aa5eaa49ab306ea82e13f8
Young Seo Kim <youngseokim7@gmail.com>
2025-08-16 11:13:25 +0900

    put set winner in socket manager

--- frontend/src/components/socket-manager.tsx
+++ frontend/src/components/socket-manager.tsx
@@ -1,10 +1,39 @@
 import { useSocketStore } from "@/stores/socket-store";
-import { useEffect, ReactNode } from "react";
+import { useEffect } from "react";
+import socket from "@/socket-config/socket";
+import type { Message } from "@/types/socket-store-types";
 
 export default function SocketManager({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const socketStore = useSocketStore();
+  useEffect(() => {
+    socket.on("connect", () => {
+      socketStore.connect();
+    });
+    socket.on(
+      "new-nomination",
+      (payload: { nominee: string; roomId: string }) => {
+        socketStore.addNominee(payload.nominee);
+      },
+    );
+    socket.on("state-change", (payload: { state: string; roomId: string }) => {
+      socketStore.setState(payload.state);
+    });
+    socket.on("new-user", (payload: { userName: string; roomId: string }) => {
+      socketStore.addUser(payload.userName);
+    });
+    socket.on("user-voted", (payload: { userName: string }) => {
+      socketStore.addVotedUser(payload.userName);
+    });
+    socket.on("new-message", (payload: Message) => {
+      socketStore.addMessage(payload);
+    });
+    socket.on("winner", (payload: string) => {
+      socketStore.setWinner(payload);
+    });
+  }, []);
   return <>{children}</>;
 }
