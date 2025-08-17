8863d7ba32d521bf7915ec16b2da2b2777e0178f
Young Seo Kim <youngseokim7@gmail.com>
2025-08-17 22:04:19 +0900

    initial page

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,16 @@
 import axios from "axios";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 export default function Home() {
-  const function 
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Input type="text" placeholder="userName" required />
+      <Input type="text" placeholder="roomCode" />
+      <Button>join room</Button>
+      <h1>or</h1>
+      <Separator />
+      <Button>create room</Button>
+    </>
+  );
 }
