9ca4d0ba7ae3a9395ff842c2c4cc2de2e0effae2
Young Seo Kim <youngseokim7@gmail.com>
2025-08-18 19:45:59 +0900

    add room-view component

--- frontend/src/app/rooms/[roomId]/page.tsx
+++ frontend/src/app/rooms/[roomId]/page.tsx
@@ -1,3 +1,5 @@
-export default function RoomPage() {
+export default async function RoomPage() {
+  const backendBase = process.env.NEXT_PUBLIC_BACKEND_URL;
+  const response = await fetch("");
   return <h1>hello</h1>;
 }
