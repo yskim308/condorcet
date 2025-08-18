e60c9e4dc8c283254c417b1b7841a754ab1dc570
Young Seo Kim <youngseokim7@gmail.com>
2025-08-18 19:53:24 +0900

    implement server-side data fetching

--- frontend/src/app/rooms/[roomId]/page.tsx
+++ frontend/src/app/rooms/[roomId]/page.tsx
@@ -1,5 +1,30 @@
-export default async function RoomPage() {
+import { redirect } from "next/navigation";
+
+export default async function RoomPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}) {
+  const roomId = await params;
   const backendBase = process.env.NEXT_PUBLIC_BACKEND_URL;
-  const response = await fetch("");
+  if (!backendBase) throw new Error("NEXT_PUBLIC_BACKEND_URL not set in .env");
+
+  const userName = localStorage.getItem("userName");
+  const response = await fetch(`${backendBase}/room/${roomId}/getRoomData`, {
+    method: "POST",
+    headers: {
+      "Content-type": "application/json",
+    },
+    body: JSON.stringify({
+      userName,
+    }),
+  });
+
+  if (!response.ok) {
+    redirect("/");
+  }
+
+  const roomData = await response.json();
+
   return <h1>hello</h1>;
 }
