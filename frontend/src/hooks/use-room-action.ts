b8bbf5f63134d148b1af87a1931b7c23559239f7
Young Seo Kim <youngseokim7@gmail.com>
2025-08-18 19:06:51 +0900

    complete implementation of joining and creation hooks

--- frontend/src/hooks/use-room-action.ts
+++ frontend/src/hooks/use-room-action.ts
@@ -5,11 +5,56 @@ import type {
   JoinRoomPayload,
 } from "@/lib/room-service";
 import { useMutation } from "@tanstack/react-query";
+import { toast } from "sonner";
+
+function onCreateSucces(hostKey: string, userName: string) {
+  localStorage.setItem("hostKey", hostKey);
+  localStorage.setItem("userName", userName);
+}
+
+function onJoinSuccess(userName: string) {
+  localStorage.setItem("userName", userName);
+}
+
+function onError(action: "join" | "create", error: unknown) {
+  let errorMessage = "error while trying to ";
+  action === "join" ? errorMessage + "join room" : errorMessage + "create room";
+  if (error instanceof Error) {
+    toast.error(errorMessage + error.message);
+    return;
+  }
+  toast.error(errorMessage);
+}
 
 export default function useRoomState() {
-  const handleCreate = useMutation({
+  const createMutation = useMutation({
     mutationFn: createRoom,
+    onSuccess: (data: CreateRoomReponse, variables: CreateRoomPayload, _) => {
+      const { hostKey } = data;
+      const { userName } = variables;
+      onCreateSucces(hostKey, userName);
+    },
+    onError: (error, _, __) => {
+      onError("create", error);
+    },
   });
 
-  const handleJoin = useMutation({});
+  const joinMutation = useMutation({
+    mutationFn: joinRoom,
+    onSuccess: (__, variables: JoinRoomPayload, _) => {
+      const { userName } = variables;
+      onJoinSuccess(userName);
+    },
+    onError: (error, _, __) => {
+      onError("join", error);
+    },
+  });
+
+  const isLoading = createMutation.isPending || joinMutation.isPending;
+  return {
+    handleCreateRoom: (payload: CreateRoomPayload) =>
+      createMutation.mutate(payload),
+    handleJoinRoom: (payload: JoinRoomPayload) => joinMutation.mutate(payload),
+    isLoading,
+  };
 }
