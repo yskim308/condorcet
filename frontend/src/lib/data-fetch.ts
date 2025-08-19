a4f626ed1230282e3217929836acd12110c29ecc
Young Seo Kim <youngseokim7@gmail.com>
2025-08-19 15:56:23 +0900

    use axios properly and propgate error to be handled by tanstack query

--- frontend/src/lib/data-fetch.ts
+++ frontend/src/lib/data-fetch.ts
@@ -12,12 +12,8 @@ interface RoomData {
 export async function fetchRoomData(roomId: string): Promise<RoomData | void> {
   const backendBase = process.env.NEXT_PUBLIC_BACKEND_URL;
   if (!backendBase) throw new Error("NEXT_PUBLIC_BACKEND_URL not set in .env");
-  try {
-    const response: RoomData = await axios.post(
-      `${backendBase}/${roomId}/getRoomdData`,
-    );
-    return response;
-  } catch (e: unknown) {
-    return;
-  }
+  const response = await axios.post<RoomData>(
+    `${backendBase}/${roomId}/getRoomdData`,
+  );
+  return response.data;
 }
