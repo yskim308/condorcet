0d7969030afd6fbafea4f0c34fd9c5e74d89029c
Young Seo Kim <youngseokim7@gmail.com>
2025-08-27 17:03:06 +0900

    implement mutation functions for adding nominess and state changes

--- frontend/src/lib/vote-service.ts
+++ frontend/src/lib/vote-service.ts
@@ -1,9 +1,25 @@
 import axios from "axios";
 import backendBase from "./backend-baseUrl";
 
-const addNominee = async (roomId: string, nominee: string, hostKey: string) => {
+export const addNominee = async (
+  roomId: string,
+  nominee: string,
+  hostKey: string,
+) => {
   await axios.post(`${backendBase}/rooms/${roomId}/nomination`, {
     hostKey: hostKey,
     nominee: nominee,
+  });
+};
+
+export const setVoting = async (roomId: string, hostKey: string) => {
+  await axios.post(`${backendBase}/rooms/${roomId}/setVoting`, {
+    hostKey: hostKey,
+  });
+};
+
+export const setDone = async (roomId: string, hostKey: string) => {
+  await axios.post(`${backendBase}/rooms/${roomId}/setDone`, {
+    hostKey: hostKey,
   });
 };
