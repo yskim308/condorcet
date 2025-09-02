22b913d722d880898e975f73e9aecde588fa56e8
Young Seo Kim <youngseokim7@gmail.com>
2025-09-02 21:32:12 +0900

    update bugs with mutations for host controls

--- frontend/src/components/host-controls/nomination-control-panel.tsx
+++ frontend/src/components/host-controls/nomination-control-panel.tsx
@@ -1,17 +1,53 @@
 "use client";
-import { useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import useVotingActions from "@/hooks/use-voting-actions";
+import { useRoomStore } from "@/stores/room-store";
+import { toast } from "sonner";
+import { useShallow } from "zustand/shallow";
 
 export default function NominationControlPanel() {
   const [input, setInput] = useState<string>("");
+
+  const { handleAddNominee, handleSetVoting } = useVotingActions();
+
+  const { hostKey, roomId } = useRoomStore(
+    useShallow((state) => ({
+      hostKey: state.hostKey,
+      roomId: state.roomId,
+    })),
+  );
+
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInput(e.target.value);
+  };
+
+  const handleAddNomineeAndClear = () => {
+    if (!roomId || !hostKey) {
+      toast.error("hostKey or roomId is not defined!");
+      return;
+    }
+    handleAddNominee({ roomId, nominee: input, hostKey });
+    setInput("");
+  };
+
+  const handleSetVotingClick = () => {
+    if (!roomId || !hostKey) {
+      toast.error("hostKey or roomId is not defined!");
+      return;
+    }
+    handleSetVoting({ roomId, hostKey });
+  };
+
   return (
     <>
       <h1 className="text-5xl text-red-400">
         ok checking, should only show on host
       </h1>
-      <Input />
-      <Button>Next State</Button>
+      <Input value={input} onChange={handleInputChange} />
+      <Button onClick={handleAddNomineeAndClear}>Add Nominee</Button>
+      <Button onClick={handleSetVotingClick}>Start Vote</Button>
     </>
   );
 }
