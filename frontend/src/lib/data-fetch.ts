446690b66e567c2cbb4a4015c3be07e9bcb3fdd5
Young Seo Kim <youngseokim7@gmail.com>
2025-09-08 09:09:24 +0900

    on query, also join the socket

--- frontend/src/lib/data-fetch.ts
+++ frontend/src/lib/data-fetch.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import backendBase from "./backend-baseUrl";
 import { NominationsMap } from "@/types/socket-store-types";
+import socket from "@/socket-config/socket";
 
 interface RoomData {
   role: string;
@@ -21,5 +22,6 @@ export async function fetchRoomData(
       userName,
     },
   );
+  socket.emit("join-room", roomId);
   return response.data;
 }
