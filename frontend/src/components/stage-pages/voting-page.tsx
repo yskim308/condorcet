e23b53e635f0257bea752da96084e495c10313ba
Young Seo Kim <youngseokim7@gmail.com>
2025-09-08 16:36:17 +0900

    create base skeleton for voting page

--- frontend/src/components/stage-pages/voting-page.tsx
+++ frontend/src/components/stage-pages/voting-page.tsx
@@ -1,7 +1,22 @@
+import { useSocketStore } from "@/stores/socket-store";
+import { useRoleStore } from "@/stores/role-store";
+import UsersContainer from "./users-container";
+
 export default function VotingPage() {
+  const nominations = useSocketStore((state) => state.nominationlist);
+  const users = useSocketStore((state) => state.users);
+  const isHost = useRoleStore((state) => state.isHost);
   return (
-    <>
-      <h1>hello from voting page</h1>
-    </>
+    <div className="container mx-auto p-4">
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {isHost && <div>placeholder for controls</div>}
+        <div className={!isHost ? "col-span-2" : ""}>
+          placeholder for drag and drop
+        </div>
+        <div>
+          <UsersContainer users={users} />
+        </div>
+      </div>
+    </div>
   );
 }
